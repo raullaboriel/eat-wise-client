826a11388c85201ec32d94b0798dbe327a600f26
Raul <86560746+raullaboriel@users.noreply.github.com>
2023-10-23 18:12:17 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { HomeComponent } from './pages/home/home.component';
+import { CardModule } from 'primeng/card';
+import { ChipModule } from 'primeng/chip';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { KnobModule } from 'primeng/knob';
+
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    InputTextModule,
+    CardModule,
+    ChipModule,
+    OverlayPanelModule,
+    BrowserAnimationsModule,
+    KnobModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
